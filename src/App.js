15617641a904cd502462428a20146582277964cd
Максим Шашков <88987688+Maks-Shashkov@users.noreply.github.com>
2024-02-09 05:30:53 +0300

    addBasket
    Добавление основного функционала в корзину. Добавление счетчика для цены и количества пицц.

--- src/App.js
+++ src/App.js
@@ -5,21 +5,15 @@ import Home from "./pages/Home";
 import Card from "./pages/Card";
 import NotFound from "./pages/NotFound";
 
-import {
-    createBrowserRouter,
-    RouterProvider,
-    Route,
-    Link, BrowserRouter, Routes,
-} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
+
 
 import './scss/app.scss'
 
 export const SearchContext = React.createContext();
-console.log(SearchContext)
 
 function App() {
     const [searchValue, setSearchValue] = React.useState('');
-    console.log(searchValue, setSearchValue)
     return (
         <div className="wrapper">
             <SearchContext.Provider value={{searchValue, setSearchValue}}>
